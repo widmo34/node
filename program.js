59cfeeabbca5ede6e5ca3d4f8c2e187edcadd3c1
Radoslaw Weidemann <walecznyopos@gmail.com>
2018-07-01 10:26:34 +0200

    Include os module

--- program.js
+++ program.js
@@ -1,30 +1,25 @@
+// here I include ans os module 
+var os = require('os');
+
+console.log(os.EOL);
 
 
-  
-  
-  
-  /*process.stdin.on('readable', function() {
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
-    var input = process.stdin.read();
-    if (input !== null) {
-        var instruction = input.toString().trim();
-        if (instruction === '/exit') {
-            process.stdout.write('Quitting app!\n');
-            process.exit();
+
+
+process.stdin.on('readable', function() {
+// metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+        var input = process.stdin.read();
+        if (input !== null) {
+            var instruction = input.toString().trim();
+            if (instruction === '/exit') {
+                process.stdout.write('Quitting app!\n');
+                console.log('your os language is ' +  process.env['lang'] + '/n/n');
+                console.log('your node version is ' + process.versions['node'] + '/n/n/n');
+                process.exit();
         } else {
             process.stderr.write('Wrong instruction!\n');
         }
     }
-
-
-
-    console.log(process.env)
-
-
-
-
 });
-*/
 
-    console.log(process.env['lang']);
-    console.log(process.versions['node']);
+
